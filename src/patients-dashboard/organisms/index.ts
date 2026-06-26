e405b6585caceb88e906d20de556f7117a7a11a1
Joa Sanchez <joassanchez03@gmail.com>
2026-06-26 19:58:35 -0300

    refactor(dashboard-layout): REFACTOR — barrel exports for FavoritesSection, PatientsSection

--- src/patients-dashboard/organisms/index.ts
+++ src/patients-dashboard/organisms/index.ts
@@ -1,2 +1,4 @@
 export { default as Header } from './Header';
 export { default as DashboardLayout } from './DashboardLayout';
+export { default as FavoritesSection } from './FavoritesSection';
+export { default as PatientsSection } from './PatientsSection';
